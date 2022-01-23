fc356d23f7c45ff8834c18abf4424cc88625f4be
SmithGomes <seagente@gmail.com>
2022-01-23 11:46:11 -0300

    Template

---
+++ personapi/target/generated-sources/annotations/one/digitalinnovation/personapi/mapper/PersonMapperImpl.java
@@ -0,0 +1,11 @@
+package one.digitalinnovation.personapi.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2022-01-23T10:53:30-0300",
+    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 11.0.13 (Ubuntu)"
+)
+public class PersonMapperImpl implements PersonMapper {
+}
